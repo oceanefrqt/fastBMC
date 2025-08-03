66f44e0a7af9a8f6d321c0b7ace8557b7868fb71
Oceane <oceane.fourquet@outlook.fr>
2025-08-03 11:15:17 +0200

    Removed old Module

--- Module/mappings.py
+++
@@ -1,4 +0,0 @@
-# mappings.py
-
-equiv_to_key = {1: (False, True), 2: (True, True), 3: (False, False), 4: (True, False)}
-equiv_to_case = {(False, True): 1, (True, True): 2, (False, False): 3, (True, False): 4}
